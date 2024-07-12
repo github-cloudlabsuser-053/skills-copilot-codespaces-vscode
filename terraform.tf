34d6688ae831bac772fd733ee4c563f65ab1898d
github-cloudlabsuser-053 <80258591+github-cloudlabsuser-053@users.noreply.github.com>
2024-07-12 12:54:33 +0000

    Copilot third commit

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,4 @@
-provider "azurerm" {
+cprovider "azurerm" {
   features {}
 }
 
@@ -8,10 +8,9 @@ resource "azurerm_resource_group" "example" {
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageacc"
+  name                     = "examplestorageaccount"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  allow_blob_public_access = true
-}c
+}
